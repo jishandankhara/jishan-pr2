bd13bd237d0c4a34c46d22b5b087e9605b6c0a6b
jishandankhara <136707738+jishandankhara@users.noreply.github.com>
2023-06-27 18:04:16 +0530

    Update PR2SHAN.C

--- PR2SHAN.C
+++ PR2SHAN.C
@@ -3,37 +3,22 @@
 #include<conio.h>
 void main(){
 int unit;
-float amount,total,;
+float amount,total-amount,surcharge;
 clrscr();
 printf("_______________________\n");
 printf("---------bill----------\n");
-printf("***********************\n");
+printf("***********%.2f",total-amount************\n");
 printf("enter electricity units:");
-scanf("%f",&a);
+scanf("%f",&unit);
 
-if(a<=50){
-    if()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if(a<=50){ 
+amount = unit*0.50;}
+    else if(unit<=250){
+     amount = 25 + (unit - 50) * 0.75;}
+}else if(unit <= 150) * 1.20;
+}else{amount = 220 +(unit - 250) *1.50;}
+surcharge = amount * 20%;
+total-amunt=amount +surcharge;
+peintf("your total bill is Rs.%.2",total-amount);
 getch();
 }
